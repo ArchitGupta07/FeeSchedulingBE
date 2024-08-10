6f519f702809c80b8091f3a3f9b142107e42257a
Architgupta07 <gupta.archit01@gmail.com>
2024-08-11 02:24:53 +0530

    commit

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://fee-scheduling.vercel.app","*"],
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE"],
     allow_headers=["*"],
