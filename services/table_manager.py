6cf4bc9946b351ca963d154a1dcd955d28bb8dca
ArchitGupta07 <gupta.archit01@gmail.com>
2024-08-12 10:32:54 +0530

    Emplty row values error managed

--- services/table_manager.py
+++ services/table_manager.py
@@ -43,6 +43,19 @@ class TableManager:
         table_name = self.generate_table_name(table_prefix)
         
         df = pd.read_excel(content)
+
+
+        for column in df.columns:
+            if pd.api.types.is_numeric_dtype(df[column]):
+                df[column].fillna(0, inplace=True)  # Fill numeric columns with 0
+            elif pd.api.types.is_string_dtype(df[column]):
+                df[column].fillna('', inplace=True)  # Fill string columns with empty string
+            elif pd.api.types.is_datetime64_any_dtype(df[column]):
+                df[column].fillna(pd.Timestamp('1970-01-01'), inplace=True)  # Fill datetime columns with a default date
+            else:
+                df[column].fillna('unknown', inplace=True)
+
+                
         hashable_cols = []
 
         col1 = self.extract_code_colname(df)
