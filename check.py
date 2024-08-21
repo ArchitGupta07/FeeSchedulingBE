3e4a8606fb9171262bddf13746eba455b328389b
ArchitGupta07 <gupta.archit01@gmail.com>
2024-08-21 16:39:47 +0530

    commit

--- check.py
+++ check.py
@@ -1,9 +1,12 @@
 
 
 
-from services.version_maintainer import VersionManager
+from services.backTracker import BackTracker
 
 
-check  = VersionManager("cell_changes")
 
-print(check.apply_new_changes("demo10_20240820_141456"))
+check = BackTracker()
+
+
+check.revert_table_changes("demo100_20240821_150037","84cf2789-1799-4884-bfee-9e81bd3e42b2")
+# check.get_updated_cell_changes("947a1659-9428-47cc-8533-26c1abd690b6")
